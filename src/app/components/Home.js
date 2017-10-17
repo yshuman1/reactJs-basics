9f738fb7fa08dd29392cb21bca93e3d354e87c89
yshuman1 <yshuman@loyalloops.com>
2017-10-17 15:28:12 -0700

    stopping 2 videos short of completion on this react tutorial because much of it has been deprecated

---
+++ src/app/components/Home.js
@@ -0,0 +1,80 @@
+import React from "react";
+import PropTypes from 'prop-types';
+
+export class Home extends React.Component {
+ constructor(props) {
+   super();
+   this.state = {
+    age: props.initialAge,
+    status: 0,
+    homeLink: props.initialLinkName
+   };
+   setTimeout(() => {
+     this.setState({
+      status: 1
+      });
+     }, 3000);
+     console.log('Constructor');
+ }
+ componentWillMount() {
+   console.log('Component will mount');
+ }
+ componentDidMount() {
+   console.log('Component did mount');
+ }
+ componentWillReceiveProps(nextProps) {
+   console.log('Component will receive props', nextProps);
+ }
+ shouldComponentUpdate(nextProps, nextState) {
+   console.log('Should component update', nextProps, nextState);
+   return true;
+ }
+ componentWillUpdate(nextProps, nextState) {
+   console.log('Component will update', nextProps, nextState);
+ }
+ componentDidUpdate(prevProps, prevState) {
+   console.log('Component did update', prevProps, prevState)  
+ }
+ componentWillUnmount() {
+   console.log('Component will unmount');
+ }
+  onMakeOlder() {
+    this.setState({
+      age: this.state.age + 3
+    });
+ }
+
+ onChangeLink() {
+   this.props.changeLink(this.state.homeLink);
+ }
+ onHandleChange(event){
+  this.setState({
+    homeLink: event.target.value
+  })
+ }
+  render() {
+    var text = "Something!";
+    return (
+      <div>
+        <p>In a new Component!</p>
+        <p>Your name is {this.props.name}, your age is {this.state.age}</p>
+        <p>Status: {this.state.status}</p>
+        <hr/>
+        <button onClick={() => this.onMakeOlder()} className="btn btn-primary">Make me older!</button>
+        <hr/>
+        <button onClick={this.props.greet} className="btn btn-primary">Greet</button>
+        <hr/>
+        <input type="text" value={this.state.homeLink} 
+        onChange={(event) => this.onHandleChange(event)}/>
+        <button onClick={this.onChangeLink.bind(this)}className="btn btn-primary">Change Header Link</button>
+      </div>
+    );
+  }
+}
+
+Home.propTypes = {
+  name: PropTypes.string,
+  initialAge: PropTypes.number,
+  greet: PropTypes.func,
+  initialLinkName: PropTypes.string
+};
--- src/app/index.js
+++ src/app/index.js
@@ -1,1 +1,68 @@
-console.log('It works!');
+import React from "react";
+import{render} from "react-dom";
+import {Router, Route, browserHistory} from "react-router";
+import { Header } from "./components/Header";
+import { Home } from "./components/Home";
+
+
+class App extends React.Component {
+  constructor() {
+    super();    
+    this.state = {
+      homeLink: "Home",
+      homeMounted: true
+    };
+  }
+  onGreet() {
+    alert("hello!");
+  }
+  onChangeLinkNAme(newName) {
+    this.setState({
+      homeLink: newName
+    });
+  }
+  onChangeHomeMounted() {
+    this.setState({
+      homeMounted: !this.state.homeMounted
+    });
+  }
+  render() {
+    let homeCmp = "";
+    if(this.state.homeMounted) {
+      homeCmp = (
+        <Home 
+          name={"yasin"} 
+          initialAge={39} 
+          greet={this.onGreet}
+          changeLink={this.onChangeLinkNAme.bind(this)}
+          initialLinkName={this.state.homeLink}
+        />
+      );
+    }
+    var user = {
+      name: "awni",
+      hobbies: ["being angry", "cooking", "hating trump"]
+    }
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-xs-10 col-xs-offset-1">
+            <Header homeLink={this.state.homeLink}/>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-xs-10 col-xs-offset-1">
+            {homeCmp}
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-xs-10 col-xs-offset-1">
+            <button onClick={this.onChangeHomeMounted.bind(this)} className="btn btn-primary">(Un)mount home component</button>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+render(<App/>, window.document.getElementById("app"));
